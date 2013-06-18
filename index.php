70e291193e6f226bfa41afc4bb75f0fc4227fc75
Diego Gutiérrez <diego804@gmail.com>
2013-06-18 12:12:25 -0500

    otros cambios menores

--- index.php
+++ index.php
@@ -21,7 +21,8 @@ and open the template in the editor.
         var geocoder; 
         var centerChangedLast; 
         var reverseGeocodedLast; 
-        var currentReverseGeocodeResponse; 
+        var currentReverseGeocodeResponse;
+
         function initialize() { 
         var latlng = new google.maps.LatLng(11.011513148990913,-74.83911499999999); 
         var myOptions = { 
@@ -38,24 +39,19 @@ and open the template in the editor.
         function setupEvents() { 
         reverseGeocodedLast = new Date(); 
         centerChangedLast = new Date(); 
-
         setInterval(function() { 
         if((new Date()).getSeconds() - centerChangedLast.getSeconds() > 1) { 
         if(reverseGeocodedLast.getTime() < centerChangedLast.getTime()) 
         reverseGeocode(); 
         } 
         }, 1000); 
-
         google.maps.event.addListener(map, 'zoom_changed', function() { 
         document.getElementById("zoom_level").innerHTML = map.getZoom(); 
         }); 
-
         google.maps.event.addListener(map, 'center_changed', centerChanged); 
-
         google.maps.event.addDomListener(document.getElementById('crosshair'),'dblclick', function() { 
         map.setZoom(map.getZoom() + 1); 
         }); 
-
         } 
 
         function getCenterLatLngText() { 
@@ -111,7 +107,6 @@ and open the template in the editor.
         position: map.getCenter(), 
         map: map 
         }); 
-
         var text = 'Lat/Lng: ' + getCenterLatLngText(); 
         if(currentReverseGeocodeResponse) { 
         var addr = ''; 
@@ -122,9 +117,7 @@ and open the template in the editor.
         } 
         text = text + '<br>' + 'Dirección: <br>' + addr; 
         } 
-
         var infowindow = new google.maps.InfoWindow({ content: text }); 
-
         google.maps.event.addListener(marker, 'click', function() { 
         infowindow.open(map,marker); 
         }); 
