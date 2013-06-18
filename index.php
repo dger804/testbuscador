707c4b2a3f52dee8da4d40fdc9b14367a0106cfd
Diego Gutiérrez <diego804@gmail.com>
2013-06-18 16:14:38 -0500

    prueba con geocode q no sirve :(

--- index.php
+++ index.php
@@ -14,112 +14,56 @@ and open the template in the editor.
         <script type="text/javascript"
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWCHf6u-G9HZNxSFUgJk32_kSzPOc_eSI&sensor=false">            
         </script>
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&libraries=places"></script>
         <script type="text/javascript"> 
-        var map; 
-        var geocoder; 
-        var centerChangedLast; 
-        var reverseGeocodedLast; 
-        var currentReverseGeocodeResponse;
-
+        var map;
+        var infowindow; 
+        
         function initialize() { 
         var latlng = new google.maps.LatLng(11.011513148990913,-74.83911499999999); 
         var myOptions = { 
-        zoom: 11, 
+        zoom: 12, 
         center: latlng, 
         mapTypeId: google.maps.MapTypeId.ROADMAP 
         }; 
         map = new google.maps.Map(document.getElementById("map_canvas"), myOptions); 
-        geocoder = new google.maps.Geocoder(); 
-        setupEvents(); 
-        centerChanged(); 
+        var request ={
+            location: latlng,
+            radius:500,
+            types:['hospital']
+        };
+        infowindow = new google.maps.InfoWindows();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);  
         } 
 
-        function setupEvents() { 
-        reverseGeocodedLast = new Date(); 
-        centerChangedLast = new Date(); 
-        setInterval(function() { 
-        if((new Date()).getSeconds() - centerChangedLast.getSeconds() > 1) { 
-        if(reverseGeocodedLast.getTime() < centerChangedLast.getTime()) 
-        reverseGeocode(); 
-        } 
-        }, 1000); 
-        google.maps.event.addListener(map, 'zoom_changed', function() { 
-        document.getElementById("zoom_level").innerHTML = map.getZoom(); 
-        }); 
-        google.maps.event.addListener(map, 'center_changed', centerChanged); 
-        google.maps.event.addDomListener(document.getElementById('crosshair'),'dblclick', function() { 
-        map.setZoom(map.getZoom() + 1); 
-        }); 
-        } 
+        function callbak(results, status){
+            if(status == google.maps.places.PlacesServiceStatus.ok){
+                for (var i = 0;i < results.length; i++){
+                    createMarker(results[i]);
+                }
+            }
+        }
 
-        function getCenterLatLngText() { 
-        return '(' + map.getCenter().lat() +', '+ map.getCenter().lng() +')'; 
-        } 
+        function createMarker(place){
+            var placeLoc = place.geometry.location;
+            var marker = new google.maps.Marker({
+                map:map,
+                position: place.geometry.location
 
-        function centerChanged() { 
-        centerChangedLast = new Date(); 
-        var latlng = getCenterLatLngText(); 
-        var lat = map.getCenter().lat(); 
-        var lng = map.getCenter().lng(); 
-        document.getElementById('lat').innerHTML = lat; 
-        document.getElementById('lng').innerHTML = lng; 
-        document.getElementById('formatedAddress').innerHTML = ''; 
-        currentReverseGeocodeResponse = null; 
-        } 
+            });
+            google.maps.event.addListener(marker, 'click', function(){
+                infowindow.setContent(place.name);
+                infowindow.open(map, this);
+            });
+        }
 
-        function reverseGeocode() { 
-        reverseGeocodedLast = new Date(); 
-        geocoder.geocode({latLng:map.getCenter()},reverseGeocodeResult); 
-        } 
+        google.maps.evente.addDomListener(window, 'load',initialize);
+        autocomplete = new google.maps.places.Autocomplete(address, myOptions) 
 
-        function reverseGeocodeResult(results, status) { 
-        currentReverseGeocodeResponse = results; 
-        if(status == 'OK') { 
-        if(results.length == 0) { 
-        document.getElementById('formatedAddress').innerHTML = 'None'; 
-        } else { 
-        document.getElementById('formatedAddress').innerHTML = results[0].formatted_address; 
-        } 
-        } else { 
-        document.getElementById('formatedAddress').innerHTML = 'Error'; 
-        } 
-        } 
+        
 
-        function geocode() { 
-        var address = document.getElementById("address").value; 
-        geocoder.geocode({ 
-        'address': address, 
-        'partialmatch': true}, geocodeResult); 
-        } 
 
-        function geocodeResult(results, status) { 
-        if (status == 'OK' && results.length > 0) { 
-        map.fitBounds(results[0].geometry.viewport); 
-        } else { 
-        alert("Geocode was not successful for the following reason: " + status); 
-        } 
-        } 
-
-        function addMarkerAtCenter() { 
-        var marker = new google.maps.Marker({ 
-        position: map.getCenter(), 
-        map: map 
-        }); 
-        var text = 'Lat/Lng: ' + getCenterLatLngText(); 
-        if(currentReverseGeocodeResponse) { 
-        var addr = ''; 
-        if(currentReverseGeocodeResponse.size == 0) { 
-        addr = 'None'; 
-        } else { 
-        addr = currentReverseGeocodeResponse[0].formatted_address; 
-        } 
-        text = text + '<br>' + 'Dirección: <br>' + addr; 
-        } 
-        var infowindow = new google.maps.InfoWindow({ content: text }); 
-        google.maps.event.addListener(marker, 'click', function() { 
-        infowindow.open(map,marker); 
-        }); 
-        } 
     </script>
     </head>   
     <body onload="initialize()" onunload="GUnload()" >    
@@ -131,27 +75,23 @@ and open the template in the editor.
         ?>
            <div class="login-help">
                 <p><a href="logout.php">>>SALIR<<</a></p>
-           </div>     
-           <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script> 
+           </div>   
+
+           
             <div id="map"> 
-            <div id="map_canvas" ></div> 
-            <div id="crosshair"></div> 
+            <div id="map_canvas"></div> 
+             
+            
+
             <div class="container"> 
             <p><input type="text" id="address" placeholder="Escribe aquí tu lugar..." value="" class="input"> 
                <input type="button" value="Buscar" onclick="geocode()" class="button">
                <input type="button" value="Insertar marcador" onclick="addMarkerAtCenter()" class="button">
             </p>
             </div> 
-            <div class="coordinates"> 
-                <em class="lat">Latitud</em> 
-                <em class="lon">Longitud</em> 
-            <span id="lat"></span> 
-            <span id="lng"></span> 
-            </div> 
-                <div class="address"> 
-                <span id="formatedAddress">-</span> 
-                </div> 
-              <span id="zoom_level"></span> 
+            
+                 
+               
          <?php 
         }
         ?>
